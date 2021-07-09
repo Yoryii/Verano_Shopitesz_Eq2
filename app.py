8f9139c9756b6969d41df0c59068cea5bcce431b
JoseAngelMad <72820829+JoseAngelMad@users.noreply.github.com>
2021-07-08 22:23:01 -0500

    Update

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask,render_template
 from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 Bootstrap(app)
+
+@app.route("/")
+def inicio():
+    return render_template('principal.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
