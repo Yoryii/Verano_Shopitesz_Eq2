b506260f627b61bdc186a9773a0a21de05d83eac
Yoryii <72758508+Yoryii@users.noreply.github.com>
2021-07-11 14:38:22 -0500

    pedidos, pedidoDetalle

--- app.py
+++ app.py
@@ -11,5 +11,13 @@ def login():
 def registrarUsuario():
     return render_template('Usuarios/registrar.html')
 
+@app.route('/pedidos/pedidos')
+def consultarPedidos():
+    return render_template('Pedidos/pedidos.html')
+
+@app.route('/pedidos/verPedido')
+def verPedido():
+    return render_template('Pedidos/verPedido.html')
+
 if __name__=='__main__':
     app.run(debug=True)
