8df3b681810963bc7c34e02abd18d1d2574bf75a
Yoryii <72758508+Yoryii@users.noreply.github.com>
2021-07-11 13:46:32 -0500

    Login, registro, estilo

--- app.py
+++ app.py
@@ -2,3 +2,14 @@ from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 Bootstrap(app)
+
+@app.route('/login')
+def login():
+    return render_template('Usuarios/login.html')
+
+@app.route('/usuarios/new')
+def registrarUsuario():
+    return render_template('Usuarios/registrar.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
