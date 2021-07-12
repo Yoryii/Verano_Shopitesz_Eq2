53dadf9fbfba41f2b9d79294572df65710521525
Yoryii <72758508+Yoryii@users.noreply.github.com>
2021-07-12 16:53:45 -0500

    Rutas ordenadas

--- app.py
+++ app.py
@@ -3,18 +3,24 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 Bootstrap(app)
 
+#Rutas del sistema - inicio
+#Página principal
+@app.route('/')
+def inicio():
+    return render_template('principal.html')
+
+#Login
 @app.route('/login')
 def login():
     return render_template('Usuarios/login.html')
 
+#Rutas del sistema - fin
+
+#Rutas de Yoryi - inicio
+
 @app.route('/usuarios/new')
 def registrarUsuario():
     return render_template('Usuarios/registrar.html')
-
-@app.route('/categorias')
-def categorias():
-    return render_template('categorias.html')
-
 
 @app.route('/pedidos/pedidos')
 def consultarPedidos():
@@ -23,6 +29,10 @@ def consultarPedidos():
 @app.route('/pedidos/verPedido')
 def verPedido():
     return render_template('Pedidos/verPedido.html')
+
+#Rutas de Yoryi - fin
+
+#Rutas de Pancho - inicio
 
 @app.route('/pedidos/carrito')
 def carrito():
@@ -36,18 +46,19 @@ def tarjetas():
 def nuevaTarjeta():
     return render_template('Tarjeta/registrarTarjeta.html')
 
-
 @app.route('/usuarios/error')
 def error():
     return render_template('Usuarios/error.html')
 
-@app.route('/Error')
-def carritos():
-    return render_template('Usuarios/Error.html')
+#Rutas de Pancho - fin
 
-@app.route('/usuarios/error')
-def errore():
-    return render_template('Usuarios/error.html')
+#Rutas de JO - inicio
+
+@app.route('/categorias')
+def categorias():
+    return render_template('categorias.html')
+
+#Rutas de JO - fin
 
 if __name__=='__main__':
     app.run(debug=True)
